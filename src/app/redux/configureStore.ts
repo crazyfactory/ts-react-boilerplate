c77c53cafa336373dcf8a7d08b2752277d76e0e4
Nishchal <gautam.nishchal@gmail.com>
2017-03-27 10:44:47 +0700

    refactor(store): change require to imports for redux-logger

--- src/app/redux/configureStore.ts
+++ src/app/redux/configureStore.ts
@@ -2,10 +2,10 @@ import {History} from "history";
 const appConfig = require("../../../config/main");
 import { routerMiddleware } from "react-router-redux";
 import { applyMiddleware, compose, createStore } from "redux";
+import {createLogger} from "redux-logger";
 import thunk from "redux-thunk";
 import { IStore } from "./IStore";
 import rootReducer from "./rootReducer";
-const {createLogger} = require("redux-logger");
 
 export function configureStore(history: History, initialState?: IStore): Redux.Store<IStore> {
 
