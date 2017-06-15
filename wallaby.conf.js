e5082e06150b04392a07e1a48585eafce7bfa1df
Pai <gavinsitti@hotmail.com>
2017-06-15 15:25:56 +0700

    fix(config): wallaby

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,7 +1,16 @@
 module.exports = function () {
   return {
-    files: ['src/**/*.ts', 'src/**/*.tsx', '!src/**/*.test.ts', '!src/**/*.test.tsx'],
-    tests: ['src/**/*.test.ts', 'src/**/*.test.ts'],
+    files: [
+      // There are not only .ts files, also .tsx, .png used.
+      // also config/main.js is used from one the files
+      {pattern: 'config/main.js'},
+      {pattern: 'src/**/*.ts*'},
+      {pattern: 'src/**/*.png'},
+      {pattern: 'src/**/*.test.ts*', ignore: true}
+    ],
+    tests: [
+      {pattern: 'src/**/*.test.ts*'}
+    ],
     env: {
       type: 'node',
       runner: 'node'
