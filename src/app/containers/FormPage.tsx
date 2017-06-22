b905e6e610ca8001e82e9eb7ba092e6ee329ca79
Pai <gavinsitti@hotmail.com>
2017-06-22 15:19:54 +0700

    WIP: form

--- src/app/containers/FormPage.tsx
+++ src/app/containers/FormPage.tsx
@@ -1,36 +1,8 @@
 import {Promise} from "es6-promise";
 import * as React from "react";
 import { Field, reduxForm } from "redux-form";
-// import { aol, email, maxLength, minValue, numberType, renderField, required, tooOld } from "../helpers/FormHelper";
+import { aol, email, maxLength, minValue, numberType, renderField, required, tooOld } from "../helpers/FormHelper";
 // import {Form} from "../components";
-
-const required = (value) => (value ? undefined : "Required");
-
-const maxLength = (max) => (value) => value && value.length > max ? `Must be ${max} characters or less` : undefined;
-
-const maxLength15 = maxLength(15);
-
-const numberType = (value) => value && isNaN(Number(value)) ? "Must be a number" : undefined;
-
-const minValue = (min) => (value) => value && value < min ? `Must be at least ${min}` : undefined;
-
-const minValue18 = minValue(18);
-
-const email = (value) => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? "Invalid email address" : undefined;
-
-const tooOld = (value) => value && value > 65 ? "You might be too old for this" : undefined;
-
-const aol = (value) => value && /.+@aol\.com/.test(value) ? "Really? You still use AOL for your email?" : undefined;
-
-const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} placeholder={label} type={type} />
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-    </div>
-  </div>
-);
 
 const Form = (props) => {
   const {handleSubmit, pristine, reset, submitting} = props;
@@ -41,7 +13,7 @@ const Form = (props) => {
         type="text"
         component={renderField}
         label="Username"
-        validate={[required, maxLength15]}
+        validate={[required, maxLength(15)]}
       />
       <Field
         name="email"
@@ -56,7 +28,7 @@ const Form = (props) => {
         type="number"
         component={renderField}
         label="Age"
-        validate={[required, numberType, minValue18]}
+        validate={[required, numberType, minValue(18)]}
         warn={tooOld}
       />
       <div>
