08e4b46855c9238c62c91ef14080647aa1c7a714
Nishchal <gautam.nishchal@gmail.com>
2017-06-30 08:18:43 +0700

    test(Header): remove console errors because of missing translations

--- src/app/components/Header.test.tsx
+++ src/app/components/Header.test.tsx
@@ -4,7 +4,11 @@ import {Header, Styles} from "./Header";
 describe("<Header />", () => {
 
   const renderer = new TestHelper();
-  const component = renderer.withTranslation({locale: "en-GB", languageData: {}}).mount(Header);
+  const component = renderer.withTranslation({languageData: {
+    about: "About",
+    counter: "Counter",
+    stars: "Stars"
+  }, locale: "en-GB"}).mount(Header);
 
   it("Renders with correct style", () => {
     expect(component.find("nav")).toHaveClassName(Styles.nav);
