e9fd0821f9bfc4a68d95ef5796eefaf3d1494223
Pai <gavinsitti@hotmail.com>
2017-06-30 11:22:51 +0700

    style(formatting): header.test.tsx

--- src/app/components/Header.test.tsx
+++ src/app/components/Header.test.tsx
@@ -4,11 +4,9 @@ import {Header, Styles} from "./Header";
 describe("<Header />", () => {
 
   const renderer = new TestHelper();
-  const component = renderer.withTranslation({languageData: {
-    about: "About",
-    counter: "Counter",
-    stars: "Stars"
-  }, locale: "en-GB"}).mount(Header);
+  const component = renderer
+    .withTranslation({ languageData: { about: "About", counter: "Counter", stars: "Stars" }, locale: "en-GB" })
+    .mount(Header);
 
   it("Renders with correct style", () => {
     expect(component.find("nav")).toHaveClassName(Styles.nav);
