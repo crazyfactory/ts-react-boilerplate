c70c1be5c0ccd311a47340083d011b38bc23e9da
Pai <gavinsitti@hotmail.com>
2017-07-04 19:44:07 +0700

    refactor(configureRouter): couple things - remove listenersplugin because we use redux - remove loggerplugin because redux also logs this - add empty array middlewares

--- src/app/routes/configureRouter.ts
+++ src/app/routes/configureRouter.ts
@@ -1,16 +1,14 @@
-import createRouter, {loggerPlugin, Router} from "router5";
+import createRouter, {MiddlewareFactory, Router} from "router5";
 import browserPlugin from "router5/plugins/browser";
-import listenersPlugin from "router5/plugins/listeners";
 import routes from "./routes";
 
-export default function configureRouter(useListenersPlugin: boolean = false): Router {
+export default function configureRouter(): Router {
   const router = createRouter(routes)
-    .usePlugin(loggerPlugin)
     .usePlugin(browserPlugin({useHash: false}));
 
-  if (useListenersPlugin) {
-    router.usePlugin(listenersPlugin());
-  }
+  const middlewares: MiddlewareFactory[] = [];
+
+  router.useMiddleware(...middlewares);
 
   return router;
 }
