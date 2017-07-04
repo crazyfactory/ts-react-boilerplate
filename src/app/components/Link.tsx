14e3b4c61c5c2f09603b173763c71ac838ac2953
Pai <gavinsitti@hotmail.com>
2017-07-04 22:48:44 +0700

    WIP: implement Link

--- src/app/components/Link.tsx
+++ src/app/components/Link.tsx
@@ -1,22 +1,53 @@
 import * as React from "react";
+import {isEqual} from "lodash";
 import {connect} from "react-redux";
 import {actions} from "redux-router5";
+import {State as IRouterState} from "router5";
 
-class Link extends React.Component<any, null> {
+class Link extends React.Component<IStateToProps & IDispatchToProps & IOwnProps, null> {
   public render(): JSX.Element {
-    const {children, name, params, options} = this.props;
+    const {children, name, params, options, route} = this.props;
 
-    // const href = router.buildUrl(name, params);
+    const href = this.buildUrl(name, params);
     const onClick = (evt) => {
       evt.preventDefault();
       this.props.dispatch(actions.navigateTo(name, params, options));
     };
-    // const className = router.isActive(name, params) ? "active" : "";
+    const className = (route.name === name && (!params || isEqual(route.params, params))) ? "active" : "";
 
-    return <a {...{onClick}}>{children}</a>;
+    return <a {...{className, href, onClick}}>{children}</a>;
+  }
+
+  // todo: now only assume query params, will implement other types of params
+  private buildUrl(name: string, params: {[key: string]: string}): string {
+    let url: string = name;
+    if (params) {
+      Object.keys(params).forEach((key) => {
+        url += `?${key}=${params[key]}`;
+      });
+    }
+    return url;
   }
 }
 
-const connectedLink = connect()(Link);
+interface IStateToProps {
+  route: IRouterState;
+}
+
+interface IDispatchToProps {
+  dispatch: any;
+}
+
+interface IOwnProps {
+  name: string;
+  params?: {[key: string]: string};
+  options?: any;
+}
+
+const mapStateToProps = (state) => ({
+  route: state.router.route
+});
+
+const connectedLink = connect<IStateToProps, IDispatchToProps, IOwnProps>(mapStateToProps)(Link);
 
 export {connectedLink as Link};
