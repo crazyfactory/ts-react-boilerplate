5702cd5b1dc93cb0221f5f4694eba23191fa3b6b
paibamboo <gavinsitti@hotmail.com>
2018-08-28 15:22:05 +0700

    fix: hmr not working

--- src/client.tsx
+++ src/client.tsx
@@ -38,7 +38,7 @@ setStylesTarget(document.getElementById("styles-target"));
 
 if ((module as any).hot) {
   (module as any).hot.accept("./app/containers", () => {
-    const {NewApp} = require("./app/containers");
+    const {App: NewApp} = require("./app/containers/App");
     ReactDOM.hydrate(
       <ReactHotLoader>
         <Provider store={store}>
