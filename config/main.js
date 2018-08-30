48d110fcf3802851e620d4b208e195609023430a
paibamboo <gavinsitti@hotmail.com>
2018-08-30 14:43:15 +0700

    improve: we don't use karma anymore

--- config/main.js
+++ config/main.js
@@ -4,7 +4,6 @@ var config = {
   env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 8889,
-  karmaPort: 9876,
   ssr: true,
   sentry: {
     dsn: '', // your sentry dsn here
