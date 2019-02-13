3c45d4f5ff5b4316bbf1271fc918131ab8e98dbd
paibamboo <gavinsitti@hotmail.com>
2019-02-13 13:35:48 +0700

    chore(package): update redux-saga to 1.0.1

--- src/app/sagas/rootSaga.ts
+++ src/app/sagas/rootSaga.ts
@@ -2,7 +2,7 @@ import {all, AllEffect} from "redux-saga/effects";
 import {SettingsSaga} from "./SettingsSaga";
 import {StarsSaga} from "./StarsSaga";
 
-export default function* rootSaga(): IterableIterator<AllEffect> {
+export default function* rootSaga(): IterableIterator<AllEffect<any>> {
   yield all([
     (new SettingsSaga()).watch(),
     (new StarsSaga()).watch()
