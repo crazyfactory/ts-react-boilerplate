5c28f941800fe4333a876fe5e8b432b14798f284
paibamboo <gavinsitti@hotmail.com>
2020-05-14 12:24:38 +0700

    followup: fix redux typings

--- src/app/redux/rootReducer.ts
+++ src/app/redux/rootReducer.ts
@@ -1,11 +1,11 @@
-import {combineReducers, Reducer} from "redux";
+import {CombinedState, combineReducers, Reducer} from "redux";
 import {router5Reducer} from "redux-router5";
 import {IStore} from "./IStore";
 import {counterReducer} from "./modules/counterModule";
 import {settingsReducer} from "./modules/settingsModule";
 import {starsReducer} from "./modules/starsModule";
 
-const rootReducer: Reducer<IStore> = combineReducers<IStore>({
+const rootReducer: Reducer<CombinedState<IStore>> = combineReducers<IStore>({
   counter: counterReducer,
   router: router5Reducer,
   settings: settingsReducer,
